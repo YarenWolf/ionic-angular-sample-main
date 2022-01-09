7b66b101bf2ba61e64db8d43b7d2a9bed6028198
yoro <734700703@qq.com>
2022-01-09 22:27:00 +0800

    解决ionic-item-sliding透明背景时的滑动出现的问题

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -9,6 +9,39 @@ export class Tab1Page {
 
   public isPopover = false;
 
+  bookList = [
+    {
+      id: "1",
+      name: "book1",
+      customLeftStyle: {
+        transform : "translate3d(80px, 0px, 0px)"
+      },
+      customRightStyle: {
+        transform : "translate3d(80px, 0px, 0px)"
+      }
+    },
+    {
+      id: "2",
+      name: "book2",
+      customLeftStyle: {
+        transform : "translate3d(80px, 0px, 0px)"
+      },
+      customRightStyle: {
+        transform : "translate3d(80px, 0px, 0px)"
+      }
+    }
+  ]
+
+  customWidth = 100;
+
+  customLeftStyle = {
+    transform : "translate3d(80px, 0px, 0px)"
+  }
+
+  customRightStyle = {
+    transform : "translate3d(80px, 0px, 0px)"
+  }
+
   constructor() {}
 
   openPopover() {
@@ -17,6 +50,50 @@ export class Tab1Page {
     } else {
       this.isPopover = true;
     }
+  }
+
+  onSwipe() {
+    console.log('111');
+  }
+
+  onItemClick() {
+    console.log('itemClick');
+  }
+
+  onItemSlidingClick(e, i) {
+    console.log('onItemSlidingClick');
+    e.currentTarget.close();
+    this.bookList[i].customRightStyle.transform = `translate3d(${this.customWidth}px, 0px, 0px)`;
+    this.bookList[i].customLeftStyle.transform = `translate3d(-${this.customWidth}px, 0px, 0px)`;
+  }
+
+  onDrag(e, i) {
+
+    e.target.getOpenAmount().then(data => {
+      
+      e.target.getSlidingRatio().then(ratio => {
+
+        if (Math.abs(data) <= this.customWidth) {
+          if (ratio > 0) {
+            // item.setCssStyle("transform", "translate3d(-144px, 0px, 0px)")
+            this.bookList[i].customRightStyle.transform = `translate3d(${this.customWidth-data}px, 0px, 0px)`;
+          } else if (ratio < 0) {
+            this.bookList[i].customLeftStyle.transform = `translate3d(-${this.customWidth+data}px, 0px, 0px)`;
+          }
+        }
+      })
+      console.log(data)
+    })
+  }
+
+  onItemSlidingTouchEnd(e, i) {
+    e.currentTarget.getOpenAmount().then(data => {
+      if (Math.abs(data) <= this.customWidth/2) {
+        this.bookList[i].customRightStyle.transform = `translate3d(${this.customWidth}px, 0px, 0px)`;
+        this.bookList[i].customLeftStyle.transform = `translate3d(-${this.customWidth}px, 0px, 0px)`;
+      }
+      console.log(data)
+    })
   }
 
 }
