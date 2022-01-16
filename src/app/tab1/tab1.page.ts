71d9d72b676304fd8ad748afa63f712ce8d2f86a
yoro <734700703@qq.com>
2022-01-16 16:25:14 +0800

    实现expanable效果，完善拖动停止时的动画效果

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -14,21 +14,37 @@ export class Tab1Page {
       id: "1",
       name: "book1",
       customLeftStyle: {
-        transform : "translate3d(80px, 0px, 0px)"
+        transform : "translate3d(80px, 0px, 0px)",
+        transition: null,
       },
       customRightStyle: {
-        transform : "translate3d(80px, 0px, 0px)"
-      }
+        transform : "translate3d(80px, 0px, 0px)",
+        transition: null,
+      },
+      customBackLeftStyle: {
+        width: "0px"
+      },
+      customBackRightStyle: {
+        width: "0px"
+      },
     },
     {
       id: "2",
       name: "book2",
       customLeftStyle: {
-        transform : "translate3d(80px, 0px, 0px)"
+        transform : "translate3d(80px, 0px, 0px)",
+        transition: null
       },
       customRightStyle: {
-        transform : "translate3d(80px, 0px, 0px)"
-      }
+        transform : "translate3d(80px, 0px, 0px)",
+        transition: null
+      },
+      customBackLeftStyle: {
+        width: "0px"
+      },
+      customBackRightStyle: {
+        width: "0px"
+      },
     }
   ]
 
@@ -65,9 +81,14 @@ export class Tab1Page {
     e.currentTarget.close();
     this.bookList[i].customRightStyle.transform = `translate3d(${this.customWidth}px, 0px, 0px)`;
     this.bookList[i].customLeftStyle.transform = `translate3d(-${this.customWidth}px, 0px, 0px)`;
+    this.bookList[i].customRightStyle.transition = 'transform .6s';
+    this.bookList[i].customLeftStyle.transition = 'transform .6s';
   }
 
   onDrag(e, i) {
+
+    this.bookList[i].customRightStyle.transition = null;
+    this.bookList[i].customLeftStyle.transition = null;
 
     e.target.getOpenAmount().then(data => {
       
@@ -81,12 +102,22 @@ export class Tab1Page {
             this.bookList[i].customLeftStyle.transform = `translate3d(-${this.customWidth+data}px, 0px, 0px)`;
           }
         }
+
+        if (ratio > 0) {
+          this.bookList[i].customBackRightStyle.width = `${Math.abs(data)}px`;
+        } else if (ratio < 0) {
+          this.bookList[i].customBackLeftStyle.width = `${Math.abs(data)}px`;
+        }
+
       })
       console.log(data)
     })
   }
 
   onItemSlidingTouchEnd(e, i) {
+    this.bookList[i].customRightStyle.transition = 'transform 500ms cubic-bezier(.36, .66, .04, 1)';
+    this.bookList[i].customLeftStyle.transition = 'transform 500ms cubic-bezier(.36, .66, .04, 1)';
+
     e.currentTarget.getOpenAmount().then(data => {
       if (Math.abs(data) <= this.customWidth/2) {
         this.bookList[i].customRightStyle.transform = `translate3d(${this.customWidth}px, 0px, 0px)`;
